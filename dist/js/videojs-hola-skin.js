db85fb1507c7d584670ca8a64bd614c6027324ad
e-neko <neko.catus@gmail.com>
2016-02-08 05:00:50 +0200

    play button transitions, replay button graphic, rounded progress bar

--- dist/js/videojs-hola-skin.js
+++ dist/js/videojs-hola-skin.js
@@ -68,11 +68,22 @@ var play1 = 'M 21.5,18 32,25 21.5,32 21.5,32 Z';
 var play2 = 'M 19.5,18 22.5,18 22.5,32 19.5,32 Z';
 var pause1 = 'M 21.5,18 24.5,25 24.5,25 21.5,32 Z';
 var pause2 = 'M 27.5,18 30.5,18 30.5,32 27.5,32 Z';
+var replay1 = ['M 889.68 166.32c-93.608-102.216-228.154-166.32',
+    '-377.68-166.32-282.77 0-512 229.23-512 512h96c0-229.75 186.25-416 ',
+    '416-416 123.020 0 233.542 53.418 309.696 138.306l-149.696 149.694',
+    'h352v-352l-134.32 134.32z'].join('');
+var replay2 = ['M 928 512c0 229.75-186.25 416-416 416-123.020 ',
+    '0-233.542-53.418-309.694-138.306l149.694-149.694h-352v352l134.32',
+    '-134.32c93.608 102.216 228.154 166.32 377.68 166.32 282.77 0 512',
+    '-229.23 512-512h-96z'].join('');
 var morph_html = [
     '<svg height="3em" width="3em" viewBox="10 10 30 30">',
-        '<g id="morph">',
-            '<path d="M 19.5,18 22.5,18 22.5,32 19.5,32 Z"/>',
-            '<path d="M 27.5,18 30.5,18 30.5,32 27.5,32 Z"/>',
+        '<g id="move">',
+            '<g id="morph">',
+                '<path d="M 19.5,18 22.5,18 22.5,32 19.5,32 Z"/>',
+                '<path d="M 27.5,18 30.5,18 30.5,32 27.5,32 Z"/>',
+            '</g>',
+            '<use xlink:href="#morph" x="-23" y="0"/>',
         '</g>',
     '</svg>'].join('');
 var umorph_html = [
@@ -80,8 +91,9 @@ var umorph_html = [
         '<use id="umorph" xlink:href="#morph" x="0" y="0"/>',
     '</svg>'].join('');
 
-HolaSkin.prototype.set_play_button_state = function(btn_svg, paused){
+HolaSkin.prototype.set_play_button_state = function(btn_svg, paused, ended){
     var intv = this.intv;
+    var _this = this;
     var steptotal = this.steptotal;
     var stagger = this.stagger;
     function mk_transition(from, to, steps){
@@ -114,11 +126,23 @@ HolaSkin.prototype.set_play_button_state = function(btn_svg, paused){
     var stepcnt = 0, stepcnt1 = 0;
     if (intv)
         clearInterval(intv);
+    if (ended)
+    {
+        bars[1].setAttribute('d', replay1);
+        bars[1].setAttribute('transform',
+            'scale(0.012) translate(1560, 1560)');
+        bars[0].setAttribute('d', replay2);
+        bars[0].setAttribute('transform',
+            'scale(0.012) translate(1560, 1560)');
+        return;
+    }
+    bars[1].removeAttribute('transform');
+    bars[0].removeAttribute('transform');
     if (paused)
     {
         var mk_path3 = mk_transform(play2, play1, steptotal);
         var mk_path4 = mk_transform(pause2, pause1, steptotal);
-        intv = setInterval(function(){
+        this.intv = setInterval(function(){
             if (stepcnt < steptotal)
                 bars[1].setAttribute('d', mk_path4());
             if (stepcnt >= stagger)
@@ -126,8 +150,8 @@ HolaSkin.prototype.set_play_button_state = function(btn_svg, paused){
             stepcnt++;
             if (stepcnt >= steptotal+stagger)
             {
-                clearInterval(intv);
-                intv = 0;
+                clearInterval(_this.intv);
+                _this.intv = 0;
             }
         }, 20);
     }
@@ -135,7 +159,7 @@ HolaSkin.prototype.set_play_button_state = function(btn_svg, paused){
     {
         var mk_path1 = mk_transform(play1, play2, steptotal);
         var mk_path2 = mk_transform(pause1, pause2, steptotal);
-        intv = setInterval(function(){
+        this.intv = setInterval(function(){
             if (stepcnt < steptotal)
                 bars[0].setAttribute('d', mk_path1());
             if (stepcnt >= stagger)
@@ -143,8 +167,8 @@ HolaSkin.prototype.set_play_button_state = function(btn_svg, paused){
             stepcnt++;
             if (stepcnt >= steptotal+stagger)
             {
-                clearInterval(intv);
-                intv = 0;
+                clearInterval(_this.intv);
+                _this.intv = 0;
             }
         }, 20);
     }
@@ -169,6 +193,9 @@ HolaSkin.prototype.init = function(){
     })
     .on('pause', function(){
         _this.set_play_button_state(morph, true);
+    })
+    .on('ended', function(){
+        _this.set_play_button_state(morph, true, true);
     });
     _this.set_play_button_state(morph, player.paused());
 };
@@ -181,7 +208,7 @@ HolaSkin.prototype.dispose = function(){
 var defaults = {
     className: 'vjs5-hola-skin',
     css: '/css/videojs-hola-skin.css',
-    ver: 'ver=0.0.1-5'
+    ver: 'ver=0.0.1-6'
 };
 
 // VideoJS plugin register
