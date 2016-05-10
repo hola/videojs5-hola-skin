927f6925d00b509b48c6ec19d2d466a1c3dfe812
norlin <alexey@norlin.guru>
2016-05-10 12:02:06 +0300

    Update build

--- dist/js/videojs-hola-skin.js
+++ dist/js/videojs-hola-skin.js
@@ -232,7 +232,7 @@ HolaSkin.prototype.dispose = function(){
 var defaults = {
     className: 'vjs5-hola-skin',
     css: '/css/videojs-hola-skin.css',
-    ver: 'ver=0.0.2-20'
+    ver: 'ver=0.0.2-21'
 };
 
 // VideoJS plugin register
