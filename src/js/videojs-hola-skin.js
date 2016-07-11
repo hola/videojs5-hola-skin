38740b796ce0f9a38be11ebc55289c48350909fa
e-neko <neko.catus@gmail.com>
2016-07-11 08:11:09 +0300

    new button graphics and animations

--- src/js/videojs-hola-skin.js
+++ src/js/videojs-hola-skin.js
@@ -74,43 +74,39 @@ HolaSkin.prototype.resize = function(){
         remove_class_name(this.el, vjs_large);
 };
 
-// XXX michaelg: taken from mp_video.js but play mode adjusted 2px right
 // play/pause curves and transform
-var play1 = 'M 21.5,18 32,25 21.5,32 21.5,32 Z';
-var play2 = 'M 19.5,18 22.5,18 22.5,32 19.5,32 Z';
-var pause1 = 'M 21.5,18 24.5,25 24.5,25 21.5,32 Z';
-var pause2 = 'M 27.5,18 30.5,18 30.5,32 27.5,32 Z';
-var replay = 'M8.661,0.001c0.006,0,0.01,0,0.01,0c0.007,0,0.007,0,0.011,'
-    +'0c0.002,0,0.007,0,0.009,0 c0,0,0,0,0.004,0c0.019-0.002,0.027,0,0.'
-    +'039,0c2.213,0,4.367,0.876,5.955,2.42l1.758-1.776c0.081-0.084,0.209'
-    +'-0.11,0.314-0.065c0.109,0.044,0.186,0.152,0.186,0.271l-0.294,6.066h'
-    +'-5.699c-0.003,0-0.011,0-0.016,0c-0.158,0-0.291-0.131-0.291-0.296 '
-    +'c0-0.106,0.059-0.201,0.146-0.252l1.73-1.751c-1.026-0.988-2.36-1.529'
-    +'-3.832-1.529c-2.993,0.017-5.433,2.47-5.433,5.51 c0.023,2.978,2.457,'
-    +'5.4,5.481,5.422c1.972-0.106,3.83-1.278,4.719-3.221l2.803,1.293l-0.019'
-    +',0.039 c-1.92,3.713-4.946,5.277-8.192,4.944c-4.375-0.348-7.848-4.013'
-    +'-7.878-8.52C0.171,3.876,3.976,0.042,8.661,0.001z';
+var play1 = 'M 0,0 0,20 14,10 Z';
+var play2 = 'M 0,0 0,20 0,10 Z';
+var pause1 = 'M 0,0 0,0 0,20 0,20 M 0,0 0,0 0,20 0,20 Z';
+var pause2 = 'M 0,0 5,0 5,20 0,20 M 9,0 14,0 14,20 9,20 Z';
+var replay = '';
 var morph_html = [
-    '<svg height="3em" width="3em" viewBox="10 10 30 30">',
+    '<svg height="3em" width="3em" viewBox="-13 -10 40 40">',
         '<g id="move">',
             '<g id="{morph}">',
-                '<path d="M 19.5,18 22.5,18 22.5,32 19.5,32 Z"/>',
-                '<path d="M 27.5,18 30.5,18 30.5,32 27.5,32 Z"/>',
+                '<path d="M 0,0 5,0 5,20 0,20 M 9,0 14,0 14,20 9,20 Z"/>',
+                '<path d="M 0,0 0,20 14,10 Z"/>',
+                '<g class="replay">',
+                    '<g class="arrow">',
+                        '<path class="st1" d="M50,50.3c-3.6,3.5-8.5,5.7-14,5.7c-11,0-20-9-20-20s9-20,20-20s20,9,20,20"/>',
+                        '<polygon class="st2" points="64,36 48,36 56,46"/>',
+                    '</g>',
+                '</g>',
             '</g>',
         '</g>',
     '</svg>'].join('');
 var umorph_html = [
-    '<svg width="100%" height="100%" viewBox="10 10 30 30">',
+    '<svg width="100%" height="100%" viewBox="-13 -10 40 40">',
         '<use id="u{morph}" xlink:href="#{morph}" x="0" y="0"/>',
     '</svg>'].join('');
 
-var volume_icon_svg = '<svg height="3em" width="3em" viewBox="-3 -6 30 30">'
-    +'<polygon points="10,0 10,18 4.5,13 0,13 0,5 4.5,5"/>'
-    +'<polygon class="volume-level-0" points="20.5,6.2 19.1,4.8 16.2,7.6 13.4,4.8 12,6.2 14.8,9 12,11.8 13.4,13.2 16.2,10.4 19.1,13.2 20.5,11.8 17.7,9"/>'
+var volume_icon_svg = '<svg height="3em" width="3em" viewBox="-5 -7 30 30">'
+    +'<polygon points="4,5 4,5 0,5 0,11 4,11 4,11 8,16 8,0"/>'
+    +'<polygon class="volume-level-0" points="11.5,4 10,5.5 12.5,8 10,10.5 11.5,12 14,9.5 16.5,12 18,10.5 15.5,8 18,5.5 16.5,4 14,6.5"/>'
     +'<g>'
-        +'<path class="volume-level-1" d="M 13.6,6.3 L 12,7.7 C 12.3,8,12.5,8.5,12.5,9 s -0.2,1 -0.5,1.3 l 1.6,1.3 c 0.6 -0.7,1 -1.6,1 -2.7  C 14.6,8,14.2,7.1,13.6,6.3 z"/>'
-        +'<path class="volume-level-2" d="M 16.8,3.7 L 15.2,5 c 0.9,1.1,1.5,2.5,1.5,4 s -0.6,2.9 -1.5,4 l 1.6,1.3 c 1.3 -1.4,2 -3.3,2 -5.3 S 18,5.1,16.8,3.7 z"/>'
-        +'<path class="volume-level-3" d="M 20,1 l -1.6,1.3 c 1.6,1.8,2.5,4.1,2.5,6.7 s -1,4.9 -2.5,6.7 L 20,17 c 1.9 -2.2,3 -4.9,3 -8 C 23,5.9,21.9,3.2,20,1 z"/>'
+        +'<path class="volume-level-1" d="M10,4.6v6.9c1.2-0.7,2-2,2-3.4S11.2,5.2,10,4.6z"/>'
+        +'<path class="volume-level-2" d="M16,8c0-2.2-0.9-4.2-2.3-5.6L12,3.6c1.2,1.1,2,2.7,2,4.4c0,1.8-0.8,3.3-2,4.4l1.7,1.2C15.1,12.2,16,10.2,16,8z"/>'
+        +'<path class="volume-level-3" d="M16.9,0l-1.6,1.2C17,3,18,5.4,18,8c0,2.6-1,5-2.7,6.8l1.6,1.2c1.9-2.1,3.1-4.9,3.1-8C20,4.9,18.8,2.1,16.9,0z"/>'
     +'</g>'
 +'</svg>';
 
@@ -158,8 +154,20 @@ HolaSkin.prototype.set_play_button_state = function(btn_svg, state){
         clearInterval(intv);
     if (state=='ended')
     {
-        umorph.setAttribute('transform', 'translate(16.3, 16.5)');
-        btn_svg.parentNode.setAttribute('transform', 'translate(16.3, 16.5)');
+        umorph.setAttribute('transform', 'translate(0, 0)');
+        // XXX michaelg workaround: chrome sometimes won't re-render <use> tag
+        // when the xlinked group is updated by css. This forces the update
+        // by applying transform to one of the invisible elements of xlinked
+        // group.
+        var render_cnt = 5;
+        var render_intv = setInterval(function(){
+            bars[0].setAttribute('transform', 'rotate('+render_cnt+'deg)');
+            render_cnt--;
+            if(render_cnt<0)
+                clearInterval(render_intv);
+        }, 100);
+        // end workaround
+        btn_svg.parentNode.setAttribute('transform', 'translate(0, 0)');
         bars[0].setAttribute('d', replay);
         bars[1].setAttribute('display', 'none');
         return;
