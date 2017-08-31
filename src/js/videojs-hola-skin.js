b7072457f448b1090bcb30eb8cefd4c5c0d825ed
Andrey Kashta <andrey@hola.org>
2017-08-31 14:10:58 +0300

    change big play button icon, show poster after video ends

--- src/js/videojs-hola-skin.js
+++ src/js/videojs-hola-skin.js
@@ -29,6 +29,15 @@ var exit_fullscreen_svg = '<svg height="100%" width="100%" viewBox="0 0 36 36">'
     +'<g><path d="m 14,14 -4,0 0,2 6,0 0,-6 -2,0 0,4 0,0 z"/></g>'
     +'<g><path d="m 22,14 0,-4 -2,0 0,6 6,0 0,-2 -4,0 0,0 z"/></g>'
     +'</svg>';
+var play_button_svg = '<svg height="100%" width="100%" viewBox="0 14 96 68">'
+    +'<path d="M96,44.3v7.3c-0.1,7.7-1,15.5-1,15.5s-0.9,6.6-3.8,9.5c-3.6,3.8-'
+    +'7.7,3.8-9.6,4c-13.4,1-33.5,0.9-33.5,0.9 c-0.8,0-25-0.2-32.5-0.9c-2.1-0.4'
+    +'-6.9-0.3-10.6-4.1c-2.9-2.9-3.8-9.5-3.8-9.5s-1-7.7-1.1-15.5v-7.3c0.2-7.8,'
+    +'1.1-15.5,1.1-15.5 s0.9-6.6,3.8-9.5c3.6-3.8,7.7-3.8,9.6-4.1c13.4-1,33.5-'
+    +'0.9,33.5-0.9s20.1-0.1,33.5,0.9c1.9,0.2,5.9,0.2,9.6,4.1 c2.9,2.9,3.8,9.5,'
+    +'3.8,9.5S95.9,36.6,96,44.3z M38.3,61.4L64,47.9L38.3,34.4V61.4z"/>'
+    +'<polygon points="64,47.9 38.3,61.4 38.3,34.4" fill="#fff"/>'
+    +'</svg>';
 var slider_gaps = '<div class="vjs-slider-gap-left"></div>'
     +'<div class="vjs-slider-gap-right"></div>';
 
@@ -132,8 +141,14 @@ HolaSkin.prototype.update_state = function(player){
     player.bigPlayButton.toggleClass(replay_classname, this.is_ended);
     player.toggleClass('vjs-pos-ended', this.is_ended && this.has_played);
     player.toggleClass('vjs-pos-started', this.has_played);
+    var poster = player.posterImage.el();
     if (this.is_ended)
+    {
         player.controlBar.playToggle.controlText('Replay');
+        setTimeout(function(){ poster.style.opacity = 1; });
+    }
+    else
+        poster.style.opacity = 0;
 };
 
 HolaSkin.prototype.dispose = function(){
@@ -207,6 +222,16 @@ FullscreenToggle.prototype.createEl = function(){
 FullscreenToggle.prototype.updateHint = function(){
     this.controlText(this.player_.isFullscreen() ? 'Exit full screen' :
         'Full screen');
+};
+
+var BigPlayButton = vjs.getComponent('BigPlayButton');
+BigPlayButton.prototype.createEl = function(){
+    var el = Button.prototype.createEl.call(this);
+    el.appendChild(vjs.createEl('div', {
+        className: 'vjs-button-icon',
+        innerHTML: play_button_svg,
+    }));
+    return el;
 };
 
 vjs.registerComponent('ControlsWatermark', vjs.extend(Button, {
